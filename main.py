f8dfe2cef691b169eb6224d2f03bee65b8247b5f
Paul Slinkard <slinkardpaul50@gmail.com>
2025-07-17 14:11:51 -0500

    Reads the csv, then converts to a list for ease of use. Checks the player's input to check if it is a state.  If it is a state then we get the coordinates, sent the pen to those coordinates and have it to write the name of the state.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+import pandas as pd 
+
 
 screen = turtle.Screen()
 screen.title('U.S. State Game')
@@ -8,7 +10,21 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-player_guess = screen.textinput(title='Guess a State', prompt='Please, enter your guess.')
+
+
+player_guess = screen.textinput(title='Guess a State', prompt='Please, enter your guess.').capitalize()
+
+data = pd.read_csv('50_states.csv')
+new_data = data.state.to_list()
+
+if player_guess in new_data:
+    pen = turtle.Turtle()
+    pen.hideturtle()
+    pen.penup()
+    location = data[data.state == player_guess]
+    pen.goto(x= location.x.item(),y= location.y.item())
+    pen.write(location.state.item())
+    
 
 
 
