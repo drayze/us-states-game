ed2203c6e725f7b59f74dd6328e5d638c7fe1b7a
Paul Slinkard <slinkardpaul50@gmail.com>
2025-07-17 14:34:32 -0500

    DId some rearranging of lines. changed the capitalize to title for the multiple word state names.  Added a while loop to keep playing and an empty list to keep track of the player's guesses.

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
-
-
-
-player_guess = screen.textinput(title='Guess a State', prompt='Please, enter your guess.').capitalize()
-
 data = pd.read_csv('50_states.csv')
 new_data = data.state.to_list()
+guesses_taken = []
 
-if player_guess in new_data:
-    pen = turtle.Turtle()
-    pen.hideturtle()
-    pen.penup()
-    location = data[data.state == player_guess]
-    pen.goto(x= location.x.item(),y= location.y.item())
-    pen.write(location.state.item())
+while len(guesses_taken) < 50:
+    
+    player_guess = screen.textinput(title='Guess a State', prompt='Please, enter your guess.').title()
+
+    if player_guess in new_data:
+        guesses_taken.append(player_guess)
+        pen = turtle.Turtle()
+        pen.hideturtle()
+        pen.penup()
+        location = data[data.state == player_guess]
+        pen.goto(x= location.x.item(),y= location.y.item())
+        pen.write(location.state.item())
     
 
 
 
-turtle.mainloop()
+screen.exitonclick()
